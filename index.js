78fd3dadf124d5a0ce442e09cd9e87ecaef82b45
Collin <cneill.m@gmail.com>
2019-01-12 17:26:34 -0500

    added click events

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // your code goes here ...
+"use strict";
+
+//Elements in HTML
+const buttons = document.getElementsByTagName('button');
+const addButton = document.getElementsByTagName('button')[0];//document.getElementsByClassName('add');
+const submitButton = document.getElementsByTagName('button')[1];
+const myForm = document.getElementsByTagName('form');
+const ageInput = document.getElementsByTagName('age');
+const relationShipSelector = document.getElementsByTagName('section');
+const smokerCheckbox = document.getElementsByName('smoker');
+
+
+//Additional Elements add via DOM
+var removeButton = document.createElement("BUTTON");
+var removeLabel = document.createTextNode("remove");
+removeButton.appendChild(removeLabel);
+document.body.appendChild(removeButton);
+
+
+
+
+
+//Button click events
+
+removeButton.onclick = function(event){
+    alert("remove");
+};
+
+addButton.onclick = function(event){
+    alert("add");   
+};
+
+submitButton.onclick = function(event){
+    alert("submit");
+};
+
+    
