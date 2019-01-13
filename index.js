9e7425bdce2a2c77733aff5a6ea64deb4da059d2
Collin <cneill.m@gmail.com>
2019-01-12 22:41:48 -0500

    added list workings

--- index.js
+++ index.js
@@ -5,13 +5,14 @@
 const buttons = document.getElementsByTagName('button');
 const addButton = document.getElementsByTagName('button')[0];//document.getElementsByClassName('add');
 const submitButton = document.getElementsByTagName('button')[1];
-const myForm = document.getElementsByTagName('form');
-const ageInput = document.getElementsByTagName('age');
-const relationShipSelector = document.getElementsByTagName('section');
-const smokerCheckbox = document.getElementsByName('smoker');
+const myForm = document.getElementsByTagName('form')[0];
+
 
 
 //Additional Elements add via DOM
+addButton.type = 'button';//making add button a button and not the defualt form submit
+
+
 var removeButton = document.createElement("BUTTON");
 var removeLabel = document.createTextNode("remove");
 removeButton.appendChild(removeLabel);
@@ -21,14 +22,76 @@ document.body.appendChild(removeButton);
 
 
 
-//Button click events
 
+
+//validation
+
+//Data POCO
+function Person(age, relationship, smoker){
+    this.age = age;
+    this.relationship = relationship;
+    this.smoker = smoker;
+};
+
+var HouseHold = [];
+
+
+//Display HouseHold
+function AddToDisplay(person)
+{
+    var ol = document.getElementsByClassName("household")[0];
+    var newLI = document.createElement("li");
+    var text = document.createTextNode("Age: " + person.age + " Relation: " + person.relationship + " Smoker: " + person.smoker);
+    var checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    newLI.appendChild(text);
+    newLI.appendChild(checkBox);
+    ol.appendChild(newLI);
+}
+
+//Cleanup
+function Cleanup(){
+    document.getElementsByName("age")[0].value = '';
+    document.getElementsByName('rel')[0].value = '';
+    document.getElementsByName('smoker')[0].checked= false;
+}
+
+
+//Button click events
 removeButton.onclick = function(event){
     alert("remove");
 };
 
 addButton.onclick = function(event){
-    alert("add");   
+    var age = document.getElementsByName("age")[0].value;
+
+    if(isNaN(age) || age < 1){
+        alert("enter real age");
+    }
+
+    var relationship = document.getElementsByName('rel')[0].value;
+
+    /*
+    if(isNaN(relationship)){
+        alert('relationship is required');
+    }
+    */
+
+    var smoker = document.getElementsByName('smoker')[0].checked;
+
+
+    var person  = new Person(age, relationship, smoker);
+
+    console.log(person);
+
+    HouseHold.push(person);
+
+    console.log(HouseHold);
+
+
+    AddToDisplay(person);
+
+    Cleanup();
 };
 
 submitButton.onclick = function(event){
